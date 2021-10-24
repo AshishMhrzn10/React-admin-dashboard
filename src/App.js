ec7287465d6f88372c351c8d965d31ab7faf3e3d
Ashish Maharjan <amaharjan1032@gmail.com>
2021-10-24 13:37:36 +0545

    Single user pagedone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
           </Route>
         </Switch>
       </div>
